5541d1390795780a2f40f788c893e2a08800c441
Breno <sbreno101@gmail.com>
2026-04-03 11:00:55 -0300

    feat: add aula05.js to project structure

---
+++ aula05.js
@@ -0,0 +1,13 @@
+// Operadores de atribuição - 05
+
+//       =  Atribuição
+//       += Adição e atribuição
+//       -= Subtração e atribuição
+//       *= Multiplicação e atribuição
+//       /= Divisão e atribuição
+//       %= Resto da divisão e atribuição
+//       **= Exponenciação e atribuição
+
+let price = 300;
+price += 50;
+console.log(price);
